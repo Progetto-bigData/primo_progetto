835f95230df47d0288411c5100a07ed75328f698
IlariaStocchi <ilaria.stocchi14@gmail.com>
2021-05-09 16:49:46 +0200

    Exercise3 with spark

---
+++ spark/es3_cartesian.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+""" spark application """
+
+import argparse
+
+from pyspark.sql import SparkSession
+
+# create parser and set its arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("--input", type=str, help="Input file path")
+parser.add_argument("--output", type=str, help="Output file path")
+
+# parse arguments
+args = parser.parse_args()
+input_filepath, output_filepath = args.input, args.output
+
+# initialize SparkSession
+spark = SparkSession.builder.appName("Spark ES3").config("spark.driver.maxResultSize", "1000M").getOrCreate()
+
+SOGLIA = 1
+
+lines_RDD_primo = spark.sparkContext.textFile(input_filepath)
+
+
+lines_RDD_primo = lines_RDD_primo.map(lambda x: (x.split(",")))
+
+lines_RDD_primo = lines_RDD_primo.map(lambda x: ([x[0], x[2], x[7]]))
+
+lines_RDD_primo = lines_RDD_primo.filter(lambda x: int(x[2][0:4]) == 2017)
+
+lines_RDD_primo = lines_RDD_primo.map(lambda x: ([tuple([x[0], x[2][5:7]]), tuple([x[1],x[2]])]))
+
+lines_RDD_primo_grouped = lines_RDD_primo.groupByKey()
+
+lines_RDD_primo_min_max = lines_RDD_primo_grouped.map(lambda x : (tuple([x[0][0]])[0], x[0][1],
+                                                      min(x[1], key = lambda k : k[1])[0], 
+                                                      max(x[1], key = lambda k : k[1])[0]))
+
+
+lines_cartesian = lines_RDD_primo_min_max.cartesian(lines_RDD_primo_min_max).filter(lambda x : x[0][0] > x[1][0] and int(x[0][1]) == int(x[1][1]))
+
+
+lines_cartesian_ticker_ticker = lines_cartesian.map(lambda x : (tuple([x[0][0], x[1][0], x[0][1]]), 
+                                                        ((float(x[0][3]) - float(x[0][2])) / float(x[0][2]))*100 ,
+                                                        ((float(x[1][3]) - float(x[1][2])) / float(x[1][2]))*100))
+
+
+lines_filtered = lines_cartesian_ticker_ticker.filter(lambda x : abs(float(x[1])-float(x[2])) <= SOGLIA)
+
+lines_grouped = lines_filtered.map(lambda x : (tuple([x[0][0], x[0][1]]), tuple([x[0][2], x[1], x[2]]))).groupByKey()
+
+result_RDD = lines_grouped.map(lambda x : (x[0], list(x[1]))).filter(lambda x : (len(x[1]) == 12)).map(lambda x : (x[0][0], x[0][1], sorted(x[1], key = lambda k : k[0])))
+
+spark.sparkContext.parallelize(result_RDD.collect()).saveAsTextFile(output_filepath)
+
